01ae8f7054cd7b837ae7e9f344451970f6e6a0b8
Akash Shakdwipeea <ashakdwipeea@gmail.com>
2019-05-16 02:26:34 +0530

    keyboard camera movements

--- app.rkt
+++ app.rkt
@@ -15,22 +15,34 @@
                             #:specular 0.6
                             #:roughness 0.2))
 
-(struct game-state (position) #:transparent)
+
+(struct game-state (pos target) #:prefab)
 (define-struct-updaters game-state)
 
-(define initial-state (lambda () (game-state origin)))
+(define (find-δv s)  (point-at (game-state-pos s)
+			       (game-state-target s)))
 
-(define lights+camera
+(define initial-state (lambda () (game-state (pos 15 0 0) origin)))
+
+
+(define (lights+camera s)
   (combine (light (pos 0 1 2) (emitted "Thistle"))
            (light (pos 0 -1 -2) (emitted "PowderBlue"))
-           (basis 'camera (point-at (pos 10 1 1) origin))))
+	   (basis 'camera (find-δv s))))
 
 
-(define (make-cube t pos)
-  (rotate-z (rotate-y (rotate-x (cube pos 1/2)
+(define (rotate-object obj t)
+  (rotate-z (rotate-y (rotate-x obj
                                 (/ t 11))
                       (/ t 13))
             (/ t 17)))
+
+(define (make-cube t pos)
+  (if (and (= (pos-x pos) 0)
+	 (= (pos-y pos) 0)
+	 (= (pos-z pos) 0))
+      (rotate-object (with-color (rgba "red" 0.9) (cube pos 1)) t)
+      (rotate-object (cube pos 1/2) t)))
 
 (define (make-cubes t positions)
   (map (λ (pos) (make-cube t pos)) positions))
@@ -44,16 +56,27 @@
                              (pos -4 0 0)
                              (pos 0 -4 0)
                              (pos 0 0 -4)))
-           lights+camera))
+           (lights+camera s)))
 
 
 (define (on-frame s n t)
-  (game-state-position-set s (pos 4 0 0)))
+  (println (game-state-pos s))
+  s)
 
 
 (define (on-key s n t k)
-  (println k)
-  s)
+  (match k
+	 ["w" (game-state-pos-update s (lambda (pos) (pos+ pos -x)))]
+	 ["a" (game-state-pos-update s (lambda (pos) (pos+ pos -y)))]
+	 ["s" (game-state-pos-update s (lambda (pos) (pos+ pos +x)))]
+	 ["d" (game-state-pos-update s (lambda (pos) (pos+ pos +y)))]
+	 [_ s]))
 
 (define (on-mouse s n t x y e)
-  s)
+  (println x)
+  (println y)
+  ;; (game-state-target-update s
+  ;; 			    (lambda (target)
+  ;; 			      (pos x y (pos-z target))))
+  s
+  )
