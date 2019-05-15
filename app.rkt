5c4e02c2cce6ed286fae73d2de383e6d8540517e
Akash Shakdwipeea <ashakdwipeea@gmail.com>
2019-05-15 20:50:44 +0530

    add game state

--- app.rkt
+++ app.rkt
@@ -1,6 +1,10 @@
 #lang racket
 
+(provide initial-state)
 (provide on-draw)
+(provide on-key)
+(provide on-mouse)
+(provide on-frame)
 
 (require pict3d
          pict3d/universe)
@@ -10,26 +14,45 @@
                             #:specular 0.6
                             #:roughness 0.2))
 
+(struct game-state (position))
+
+(define initial-state (lambda () (game-state origin)))
+
 (define lights+camera
   (combine (light (pos 0 1 2) (emitted "Thistle"))
            (light (pos 0 -1 -2) (emitted "PowderBlue"))
            (basis 'camera (point-at (pos 10 1 1) origin))))
+
+
+(define (make-cube t pos)
+  (rotate-z (rotate-y (rotate-x (cube pos 1/2)
+                                (/ t 11))
+                      (/ t 13))
+            (/ t 17)))
+
+(define (make-cubes t positions)
+  (map (λ (pos) (make-cube t pos)) positions))
  
 (define (on-draw s n t)
-  (combine (rotate-z (rotate-y (rotate-x (cube origin 1/2)
-                                         (/ t 11))
-                               (/ t 13))
-                     (/ t 17))
-           (rotate-z (rotate-y (rotate-x (cube (pos 4 0 0) 1/2)
-                                         (/ t 11))
-                               (/ t 13))
-                     (/ t 17))
-           (rotate-z (rotate-y (rotate-x (cube (pos 0 4 0) 1/2)
-                                         (/ t 11))
-                               (/ t 13))
-                     (/ t 17))
-           (rotate-z (rotate-y (rotate-x (cube (pos 0 0 4) 1/2)
-                                         (/ t 11))
-                               (/ t 13))
-                     (/ t 17))
+  (combine (make-cubes t
+                       (list origin
+                             (pos 4 0 0)
+                             (pos 0 4 0)
+                             (pos 0 0 4)
+                             (pos -4 0 0)
+                             (pos 0 -4 0)
+                             (pos 0 0 -4)))
            lights+camera))
+
+
+(define (on-frame s n t)
+  (println (game-state-position (s)))
+  s)
+
+
+(define (on-key s n t k)
+  (println k)
+  s)
+
+(define (on-mouse s n t x y e)
+  s)
--- init.rkt
+++ init.rkt
@@ -7,8 +7,30 @@
   (define draw (reloadable-entry-point->procedure
                 (make-reloadable-entry-point 'on-draw "app.rkt")))
 
+  (define on-key (reloadable-entry-point->procedure
+                  (make-reloadable-entry-point 'on-key "app.rkt")))
+
+  (define on-mouse (reloadable-entry-point->procedure
+                    (make-reloadable-entry-point
+                     'on-mouse
+                     "app.rkt")))
+
+  (define on-frame (reloadable-entry-point->procedure
+                    (make-reloadable-entry-point
+                     'on-frame
+                     "app.rkt")))
+
+  (define initial-state (reloadable-entry-point->procedure
+			 (make-reloadable-entry-point
+			  'initial-state
+			  "app.rkt")))
+
   (reload!)
 
-  (big-bang3d 0 #:on-draw draw))
+  (big-bang3d initial-state
+              #:on-draw  draw
+              #:on-frame on-frame
+              #:on-key   on-key
+              #:on-mouse on-mouse))
 
 (main)
