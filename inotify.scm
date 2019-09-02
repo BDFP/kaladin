019a9477dc65d7a618aaa8435224899f1f05297d
Akash Shakdwipeea <ashakdwipeea@gmail.com>
2019-09-02 13:20:45 +0530

    convert c struct to scheme struct

--- inotify.scm
+++ inotify.scm
@@ -63,47 +63,46 @@
 ;; inotify events ;;
 ;;;;;;;;;;;;;;;;;;;;
 
-;; the following are legal, implemented events that user-space can watch for 
-(define in-access		#x00000001)	;; file was accessed 
-(define in-modify		#x00000002)	;; file was modified 
-(define in-attrib		#x00000004)	;; metadata changed 
-(define in-close-write		#x00000008)	;; writtable file was closed 
-(define in-close-nowrite	#x00000010)	;; unwrittable file closed 
-(define in-open			#x00000020)	;; file was opened 
-(define in-moved-from		#x00000040)	;; file was moved from x 
-(define in-moved-to		#x00000080)	;; file was moved to y 
-(define in-create		#x00000100)	;; subfile was created 
-(define in-delete		#x00000200)	;; subfile was deleted 
-(define in-delete-self		#x00000400)	;; self was deleted 
-(define in-move-self		#x00000800)	;; self was moved 
 
-;; the following are legal events.  they a)re sent as needed to any watch 
-(define in-unmount		#x00002000)	;; backing fs was unmounted 
-(define in-q-overflow		#x00004000)	;; event queued overflowed 
-(define in-ignored		#x00008000)	;; file was ignored 
-
-;; helper events 			  
-(define in-close		(bitwise-ior in-close-write in-close-nowrite)) ;; close 
-(define in-move			(bitwise-ior in-moved-from in-moved-to)) ;; moves 
-
-;; special flags 			  
-(define in-onlydir		#x01000000)	;; only watch the path if it is a directory 
-(define in-dont-follow		#x02000000)	;; don't follow a sym link 
-(define in-excl-unlink		#x04000000)	;; exclude events on unlinked objects 
-(define in-mask-create		#x10000000)	;; only create watches 
-(define in-mask-add		#x20000000)	;; add to the mask of an already existing watch 
-(define in-isdir		#x40000000)	;; event occurred against dir 
-(define in-oneshot		#x80000000)	;; only send event once 
+;; the following are legal, implemented events that user-space can watch for
+(define *event-masks*
+  '((#x00000001 . in-access ) ;; file was accessed
+	(#x00000002 . in-modify ) ;; file was modified 	     
+	(#x00000004 . in-attr ) ;; metadata changed 	     
+	(#x00000008 . in-close-write ) ;; writtable file was closed 
+	(#x00000010 . in-close-nowrite ) ;; unwrittable file closed   
+	(#x00000020 . in-open ) ;; file was opened 	     
+	(#x00000040 . in-moved-from ) ;; file was moved from x     
+	(#x00000080 . in-moved-to ) ;; file was moved to y 	     
+	(#x00000100 . in-create ) ;; subfile was created 	     
+	(#x00000200 . in-delete ) ;; subfile was deleted 	     
+	(#x00000400 . in-delete-self ) ;; self was deleted 	     
+	(#x00000800 . in-move-self ) ;; self was moved 	     
+	(#x00002000 . in-unmount ) ;; backing fs was unmounted	
+	(#x00004000 . in-q-overflow) ;; event queued overflowed 
+	(#x00008000 . in-ignored ) ;; file was ignored 	   
+	))
 
 ;;
 ;; all of the events - we build the list by hand so that we can add flags in
 ;; the future and not break backward compatibility.  apps will get only the
 ;; events that they originally wanted.  be sure to add new events here!
 
-(define in-all-events  (bitwise-ior in-access      in-modify        in-attrib
-				    in-close-write in-close-nowrite in-open
-				    in-moved-from  in-moved-to      in-delete
-				    in-create      in-delete-self   in-move-self))
+(define in-all-events  4095)
+
+(define (get-mask-symbol event)
+  (car (filter (lambda (em)
+		  (= (car em) (inotify-event-mask event)))
+		*event-masks*)))
+
+;; inotify event type
+(defstruct inotify (event filename))
+
+(define (event-c->scheme c-event-struct)
+  (let ((event (cdr (get-mask-symbol c-event-struct))))
+    (if (< 0 (inotify-event-name-length c-event-struct))
+      (make-inotify event (inotify-event-name c-event-struct))
+      (make-inotify event #f))))
 
 
 (define (add-watch)
@@ -122,6 +121,7 @@
 		 (inotify-event-name-length event))
 	      (cons event evs)))
       evs)))
+
 
 (define (inotify-event-generator inotify-fd)
   (lambda ()
@@ -142,6 +142,7 @@
 
 (defproto file-change-stream
   stream: (start-watch N))
+
 
 (define (inotify-change-stream)
   (define (stream dest N k)
